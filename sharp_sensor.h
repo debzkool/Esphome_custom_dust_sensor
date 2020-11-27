c3f26aaa84bd73f173396a2050c2a09bfaafeb42
debzkool <debzkool@gmail.com>
2020-11-28 03:18:05 +0530

    Update sharp_sensor.h

--- sharp_sensor.h
+++ sharp_sensor.h
@@ -1,45 +1,49 @@
 #include "esphome.h"
 class My_dust_Sensor : public PollingComponent, public Sensor {
  public:
-  My_dust_Sensor() : PollingComponent(15000) {}  // period between updates, here 10s
+  My_dust_Sensor() : PollingComponent(5000) {}  // period between updates, here 10s
   
 
   void setup() override {
 
-    pinMode(D6, OUTPUT);     // sensor led pin         
+    pinMode(D4, OUTPUT);     // sensor led pin         
     pinMode(A0, INPUT);      // output form sensor
     
    }
 
   void update() override {
 
-    int o;
-    int value = 0;
+    int samplingTime = 280;
+    int deltaTime = 40;
+    int sleepTime = 9680;
     int numReadings = 10;
-    
+    int o = 0;
+  
+    float voMeasured = 0;
+    float calcVoltage = 0;
     float dustDensity = 0;
-    float voMeasured = 0;  
 
 
 
     for (o = 0; o < numReadings; o++){
     
-    digitalWrite(D6,LOW);              // power on the LED
-    delayMicroseconds(280);            // delay
-    value = value + analogRead(A0);    // read ADC (sensor)
-    delayMicroseconds(40);             // delay
-    digitalWrite(D6,HIGH);             // turn the LED off
-    delayMicroseconds(9680);           // delay
+    digitalWrite(D4,LOW);              // power on the LED
+    delayMicroseconds(samplingTime);            // delay
+    voMeasured = voMeasured + analogRead(A0);    // read ADC (sensor)
+    delayMicroseconds(deltaTime);             // delay
+    digitalWrite(D4,HIGH);             // turn the LED off
+    delayMicroseconds(sleepTime);           // delay
     //delay(1000);
     }
     
-    value = value / numReadings;       // smoothing
-    value = 0.17 * value - 0.1;        // calculate value (for GP2Y1010AU0F reads) 
+    voMeasured = voMeasured / numReadings;       // smoothing
+    calcVoltage = voMeasured * (5.0 / 1024.0);
+    dustDensity = 0.17 * calcVoltage - 0.1;
+
+    //value = 0.17 * value - 0.1;        // calculate value (for GP2Y1010AU0F reads) 
   
   
 
-    publish_state(value);              // publish
+    publish_state(dustDensity);              // publish
   }
 };
-
-
